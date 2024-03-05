8d5ff43aa2fd06905c82bdc3c1441f293988d95d
Krotovich Krot <for.games.straus3@gmail.com>
2024-03-05 18:01:24 +0300

    New topic learned.

--- Task01/Main.cpp
+++ Task01/Main.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
+	int** classes;
+
+	int n = 5;
+	int m = 10;
+
+	classes = new int* [n];
+	for (int i = 0; i < n; i++)	{
+		*(classes + i) = new int[m];
+	}
+
+	for (int i = 0; i < n; i++) {
+		delete[] *(classes + i);
+	}
+
+	delete[] classes;
 
 	return 0;
 }
